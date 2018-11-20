a8a125a30a57070e347a662f38d8a74901a3538b
cjy <cjy632258@hotmail.com>
2018-11-20 16:16:52 +0800

    update

--- src/Utils.php
+++ src/Utils.php
@@ -13,6 +13,7 @@ class Utils
 {
 
     protected static $singleValues = [];
+    protected static $singleValueExpires = [];
 
     /**
      * Get a value, cached in request lifetime
@@ -22,10 +23,24 @@ class Utils
      */
     public static function acquireSingleValue($key, \Closure $processor)
     {
+        if (in_array($key, static::$singleValueExpires)) {
+            if (array_key_exists($key, static::$singleValues)) {
+                unset(static::$singleValues[$key]);
+            }
+            static::$singleValueExpires = array_filter(static::$singleValueExpires, function ($v) use ($key) {
+                return $v != $key;
+            });
+        }
+
         if (array_key_exists($key, static::$singleValues)) {
             return static::$singleValues[$key];
         }
 
         return static::$singleValues[$key] = $processor();
+    }
+
+    public static function expireSingleValue($key)
+    {
+        return static::$singleValueExpires[] = $key;
     }
 }
