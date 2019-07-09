48e50d95af6636fe25197a8a48abb0bcc80d0773
caojiayuan <cjy632258@hotmail.com>
2019-07-09 16:03:10 +0800

    once

--- src/Utils.php
+++ src/Utils.php
@@ -13,15 +13,18 @@ class Utils
 {
 
     protected static $onceValues = [];
+    protected static $onceValueCreates = [];
+    protected static $onceValueTTls = [];
     protected static $onceValueExpires = [];
 
     /**
      * Get a value, cached in request lifetime
      * @param $key
      * @param \Closure $processor
+     * @param null $ttl
      * @return mixed
      */
-    public static function once($key, \Closure $processor)
+    public static function once($key, \Closure $processor, $ttl = null)
     {
         if (in_array($key, static::$onceValueExpires)) {
             if (array_key_exists($key, static::$onceValues)) {
@@ -32,8 +35,23 @@ class Utils
             });
         }
 
+        if (array_key_exists($key, self::$onceValueCreates) && array_key_exists($key, self::$onceValueTTls)) {
+            $time = self::$onceValueCreates[$key];
+            $valueTTl = self::$onceValueTTls[$key];
+            if ($time + $valueTTl <= time()) {
+                if (array_key_exists($key, static::$onceValues)) {
+                    unset(static::$onceValues[$key]);
+                }
+            }
+        }
+
         if (array_key_exists($key, static::$onceValues)) {
             return static::$onceValues[$key];
+        }
+
+        if ($ttl) {
+            self::$onceValueCreates[$key] = time();
+            self::$onceValueTTls[$key] = $ttl;
         }
 
         return static::$onceValues[$key] = $processor();
